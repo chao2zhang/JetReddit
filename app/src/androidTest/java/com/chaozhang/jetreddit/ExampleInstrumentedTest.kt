2be706146cd19dad02ba8da0a782119add671bba
Chao Zhang <cazhang@linkedin.com>
2020-09-21 08:56:27 -0700

    Change app name to JetReddit

--- app/src/androidTest/java/com/chaozhang/jetreddit/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/chaozhang/jetreddit/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
   fun useAppContext() {
     // Context of the app under test.
     val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-    assertEquals("com.chaozhang.jetpackdota", appContext.packageName)
+    assertEquals("com.chaozhang.jetreddit", appContext.packageName)
   }
 }
