b56b93a84eca2f75d53190605e6462c8c0e25e34
Chao Zhang <cazhang@linkedin.com>
2020-11-13 00:03:28 -0800

    Show number of comments

--- app/src/main/java/com/chaozhang/jetreddit/ui/home/HomeFragment.kt
+++ app/src/main/java/com/chaozhang/jetreddit/ui/home/HomeFragment.kt
@@ -4,17 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumnFor
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.chaozhang.jetreddit.data.model.Reddit
 import com.chaozhang.jetreddit.data.model.RedditListing
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -30,7 +36,9 @@ class HomeFragment : Fragment() {
   ): View? {
     return ComposeView(requireContext()).apply {
       setContent {
-        RedditListing(viewModel = homeViewModel)
+        MaterialTheme {
+          RedditListing(viewModel = homeViewModel)
+        }
       }
     }
   }
@@ -41,10 +49,26 @@ fun RedditListing(viewModel: HomeViewModel) {
   val redditListings: RedditListing? by viewModel.reddits.observeAsState()
 
   redditListings?.data?.children?.let { reddits ->
-    LazyColumnFor(items = reddits) { reddit ->
+    LazyColumnFor(items = reddits) {
+      RedditRow(reddit = it)
+    }
+  }
+}
+
+@Composable
+fun RedditRow(reddit: Reddit) {
+  Column {
+    Row {
       Text(
         text = reddit.data.title,
+        fontSize = TextUnit.Sp(20),
         modifier = Modifier.padding(8.dp)
+      )
+    }
+    Row {
+      Text(
+        text = "${reddit.data.num_comments} comment",
+        modifier = Modifier.padding(PaddingValues(start = 8.dp, end = 8.dp))
       )
     }
   }
