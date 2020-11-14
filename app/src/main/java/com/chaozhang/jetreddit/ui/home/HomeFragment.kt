d029a22a004b966ef5b5cb5f4f540eabc9545f9e
Chao Zhang <cazhang@linkedin.com>
2020-11-13 16:22:07 -0800

    Show image for each reddit

--- app/src/main/java/com/chaozhang/jetreddit/ui/home/HomeFragment.kt
+++ app/src/main/java/com/chaozhang/jetreddit/ui/home/HomeFragment.kt
@@ -4,16 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumnFor
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.unit.TextUnit
@@ -23,6 +30,7 @@ import androidx.fragment.app.viewModels
 import com.chaozhang.jetreddit.data.model.Reddit
 import com.chaozhang.jetreddit.data.model.RedditListing
 import dagger.hilt.android.AndroidEntryPoint
+import dev.chrisbanes.accompanist.coil.CoilImage
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
@@ -58,6 +66,23 @@ fun RedditListing(viewModel: HomeViewModel) {
 @Composable
 fun RedditRow(reddit: Reddit) {
   Column {
+    if (reddit.data.url != null) {
+      Surface(Modifier.heightIn(40.dp, 400.dp).fillMaxWidth()) {
+        CoilImage(
+          data = reddit.data.url,
+          loading = {
+            Box(Modifier.height(40.dp).fillMaxWidth()) {
+              CircularProgressIndicator(
+                Modifier
+                  .height(20.dp)
+                  .width(20.dp)
+                  .align(Alignment.Center)
+              )
+            }
+          }
+        )
+      }
+    }
     Row {
       Text(
         text = reddit.data.title,
@@ -67,8 +92,14 @@ fun RedditRow(reddit: Reddit) {
     }
     Row {
       Text(
+        text = "@${reddit.data.author}",
+        color = MaterialTheme.colors.secondary,
+        modifier = Modifier.padding(8.dp)
+      )
+      Text(
         text = "${reddit.data.num_comments} comment",
-        modifier = Modifier.padding(PaddingValues(start = 8.dp, end = 8.dp))
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(8.dp)
       )
     }
   }
