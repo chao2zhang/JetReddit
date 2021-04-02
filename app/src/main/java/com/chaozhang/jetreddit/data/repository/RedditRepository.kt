fa2da8edaa2c997d9b193d7b469672014bc55cc9
Chao Zhang <cazhang@linkedin.com>
2021-04-02 15:09:47 -0700

    Flow to Live Data using ktx

--- app/src/main/java/com/chaozhang/jetreddit/data/repository/RedditRepository.kt
+++ app/src/main/java/com/chaozhang/jetreddit/data/repository/RedditRepository.kt
@@ -1,5 +1,7 @@
 package com.chaozhang.jetreddit.data.repository
 
+import android.util.Log
+import com.chaozhang.jetreddit.Constants.FLOW_TAG
 import com.chaozhang.jetreddit.data.model.RedditListing
 import com.chaozhang.jetreddit.data.service.RedditService
 import kotlinx.coroutines.flow.Flow
@@ -9,6 +11,7 @@ import javax.inject.Inject
 class RedditRepository @Inject constructor(private val redditService: RedditService) {
 
     val topReddits: Flow<RedditListing> = flow {
+        Log.i(FLOW_TAG, "flow running...")
         emit(redditService.top())
     }
 }
--- app/src/main/java/com/chaozhang/jetreddit/ui/home/HomeFragment.kt
+++ app/src/main/java/com/chaozhang/jetreddit/ui/home/HomeFragment.kt
@@ -1,35 +1,15 @@
 package com.chaozhang.jetreddit.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.chaozhang.jetreddit.data.model.Reddit
-import com.chaozhang.jetreddit.data.model.RedditListing
-import com.google.accompanist.coil.CoilImage
+import com.chaozhang.jetreddit.Constants
+import com.chaozhang.jetreddit.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -37,84 +17,33 @@ class HomeFragment : Fragment() {
 
   private val homeViewModel by viewModels<HomeViewModel>()
 
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    Log.i(Constants.FLOW_TAG, "Fragment onCreate...")
+  }
+
+  override fun onStart() {
+    super.onStart()
+    Log.i(Constants.FLOW_TAG, "Fragment onStart...")
+  }
+
+  override fun onResume() {
+    super.onResume()
+    Log.i(Constants.FLOW_TAG, "Fragment onResume...")
+  }
+
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View {
-    return ComposeView(requireContext()).apply {
-      setContent {
-        MaterialTheme {
-          RedditListing(viewModel = homeViewModel)
-        }
-      }
-    }
-  }
-}
-
-@Composable
-fun RedditListing(viewModel: HomeViewModel) {
-  val redditListings: RedditListing? by viewModel.reddits.observeAsState()
-  val reddits = redditListings?.data?.children
-  if (reddits != null) {
-    LazyColumn {
-      items(
-        count = reddits.size,
-        itemContent = { index ->
-          RedditRow(reddit = reddits[index])
-        }
-      )
-    }
-  }
-}
-
-@Composable
-fun RedditRow(reddit: Reddit) {
-  Column {
-    if (reddit.data.url?.isNotBlank() == true) {
-      Surface(
-        Modifier
-          .heightIn(40.dp, 400.dp)
-          .fillMaxWidth()
-      ) {
-        CoilImage(
-          data = reddit.data.url,
-          contentDescription = null,
-          loading = {
-            Box(
-              Modifier
-                .height(40.dp)
-                .fillMaxWidth()
-            ) {
-              CircularProgressIndicator(
-                Modifier
-                  .height(20.dp)
-                  .width(20.dp)
-                  .align(Alignment.Center)
-              )
-            }
-          }
-        )
-      }
-    }
-    Row {
-      Text(
-        text = reddit.data.title,
-        fontSize = 20.sp,
-        modifier = Modifier.padding(8.dp)
-      )
-    }
-    Row {
-      Text(
-        text = "@${reddit.data.author}",
-        color = MaterialTheme.colors.secondary,
-        modifier = Modifier.padding(8.dp)
-      )
-      Text(
-        text = "${reddit.data.num_comments} comment",
-        color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(8.dp)
-      )
-    }
+    Log.i(Constants.FLOW_TAG, "Fragment onCreateView...")
+    homeViewModel.reddits.observe(viewLifecycleOwner, {
+      Log.i(Constants.FLOW_TAG, "Observer called...")
+      Toast.makeText(context, "Received data...", Toast.LENGTH_SHORT).show()
+    })
+    val view = inflater.inflate(R.layout.fragment_home, container, false)
+    Thread.sleep(500) // Simulate complex layout
+    return view
   }
 }
