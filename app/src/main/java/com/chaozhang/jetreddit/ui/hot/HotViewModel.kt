9b7b7d8c4fbe0c2810b336b426b6c05afdb35e8a
Chao Zhang <cazhang@linkedin.com>
2021-08-22 19:04:03 -0700

    Run calls on IO

--- app/src/main/java/com/chaozhang/jetreddit/ui/hot/HotViewModel.kt
+++ app/src/main/java/com/chaozhang/jetreddit/ui/hot/HotViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.liveData
 import com.chaozhang.jetreddit.data.model.RedditListing
 import com.chaozhang.jetreddit.data.repository.RedditRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import javax.inject.Inject
 
 @HiltViewModel
@@ -13,7 +14,7 @@ class HotViewModel @Inject constructor(
     private val redditRepository: RedditRepository
 ) : ViewModel() {
 
-    val reddits: LiveData<RedditListing> = liveData {
+    val reddits: LiveData<RedditListing> = liveData(context = Dispatchers.IO) {
         emit(redditRepository.hotReddits())
     }
 }
